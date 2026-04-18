df85cbdf876530a53502283286f644ff10df2183
MiniScoob <miniscoob@yandex.ru>
2026-04-18 11:53:29 +0700

    href fix

--- blog-app/src/app/ui/containers/header/header.constants.ts
+++ blog-app/src/app/ui/containers/header/header.constants.ts
@@ -1,5 +1,5 @@
 export const NAV_LINKS = [
-  { href: '/', label: 'Главная' },
-  { href: '/blog', label: 'Блог' },
+  { href: '', label: 'Главная' },
+  { href: 'blog', label: 'Блог' },
   { href: '#contacts', label: 'Контакты' },
 ];
